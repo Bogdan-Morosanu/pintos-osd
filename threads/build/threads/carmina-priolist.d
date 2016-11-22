c0cea0fc0152c246a72adbf71cb11cfd707aa179
Bogdan-Morosanu <bogdan.moro.mail@gmail.com>
2016-11-22 19:12:14 +0200

    added arch linux binaries to test ubuntu 16 compatibility

---
+++ threads/build/threads/carmina-priolist.d
@@ -0,0 +1,4 @@
+threads/carmina-priolist.o: ../../threads/carmina-priolist.c \
+ ../../threads/carmina-priolist.h ../../threads/thread.h \
+ ../../lib/debug.h ../../lib/kernel/list.h ../../lib/stdbool.h \
+ ../../lib/stddef.h ../../lib/stdint.h
---
+++ threads/build/threads/moro-donation.d
@@ -0,0 +1,5 @@
+threads/moro-donation.o: ../../threads/moro-donation.c \
+ ../../threads/moro-donation.h ../../threads/synch.h \
+ ../../lib/kernel/list.h ../../lib/stdbool.h ../../lib/stddef.h \
+ ../../lib/stdint.h ../../threads/thread.h ../../lib/debug.h \
+ ../../threads/malloc.h
