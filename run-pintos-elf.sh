3bdaa39edefdd89ce503f55913fe7b2d40b32200
Bogdan-Morosanu <bogdan.moro.mail@gmail.com>
2016-12-05 06:36:41 +0200

    added script to run executable in pintos in one go

---
+++ run-pintos-elf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# -ne "2" ];
+then
+    echo "usage $0 <path to binary folder> <binary elf name>"
+    exit
+fi
+
+
+# source the pintos paths needed
+. ~/setup-shells/pintos-setup.sh 
+cd userprog
+make
+cd build
+pintos --filesys-size=2 -p "$1/$2" -a "$2.bin" -- -f -q run "$2.bin"
