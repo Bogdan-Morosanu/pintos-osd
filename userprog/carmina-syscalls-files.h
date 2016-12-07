296472663b228aa2fa520ec8e4a1674351fa243d
carminacodre <carminacodre@gmail.com>
2016-12-07 23:40:32 +0200

    modified:   userprog/carmina-syscalls-files.h 	modified:   userprog/commons-process.h 	examples/Makefile~ 	examples/open-normal.c 	userprog/1 	userprog/carmina-syscalls-files.c~ 	userprog/syscall.c~ 	userprog/test-out.txt 	utils/ 	vm/

--- userprog/carmina-syscalls-files.h
+++ userprog/carmina-syscalls-files.h
@@ -6,6 +6,9 @@
 #include <lib/kernel/list.h>
 #include "commons-process.h"
 
+#define MAX_FILE_NAME 15
+#define MAX_NO_OPENED_FILES 32
+
 struct user_file{
 	struct list_elem elem;
 	int fd; //file descriptor
@@ -15,5 +18,10 @@ struct user_file{
 #endif // carmina-syscallsfiles.h
 
 int handle_open(const char *name);
-
+int handle_close(int fd);
+int handle_seek(int fd, int pos);
+int handle_tell(int fd);
+int handle_write(int fd,char *buf,unsigned size);
+int handle_read(int fd, char *buf, unsigned size);
+bool handle_create(char *file_name, unsigned initial_size);
 
