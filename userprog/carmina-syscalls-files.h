ac401ee24db7cd2b4973a4411f4510788194f936
Bogdan-Morosanu <bogdan.moro.mail@gmail.com>
2016-12-08 12:43:33 +0200

    reverted accidental rewriting to carmina-syscall-files.h

--- userprog/carmina-syscalls-files.h
+++ userprog/carmina-syscalls-files.h
@@ -2,6 +2,7 @@
 #define CARMINA_SYSCALLSFILES_H
 
 #include <filesys/file.h>
+#include <filesys/filesys.h>
 #include <lib/kernel/list.h>
 #include "commons-process.h"
 
@@ -17,5 +18,10 @@ struct user_file{
 #endif // carmina-syscallsfiles.h
 
 int handle_open(const char *name);
-
+int handle_close(int fd);
+int handle_seek(int fd, int pos);
+int handle_tell(int fd);
+int handle_write(int fd,char *buf,unsigned size);
+int handle_read(int fd, char *buf, unsigned size);
+bool handle_create(char *file_name, unsigned initial_size);
 
