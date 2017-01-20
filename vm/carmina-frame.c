f746743c3bd652e79f94aa85394b93fd0da96148
Bogdan-Morosanu <bogdan.moro.mail@gmail.com>
2017-01-20 03:57:08 +0200

    modified:   userprog/process.c 	modified:   vm/common-supp-pd.c 	modified:   vm/moro-common.h 	modified:   vm/moro-lazy-loaded.c 	modified:   vm/moro-mmap.c 	modified:   vm/moro-mmap.h

--- vm/carmina-frame.c
+++ vm/carmina-frame.c
@@ -7,14 +7,18 @@
 
 #include "carmina-frame.h"
 #include <stdio.h>
-#include "threads/synch.h"
-#include "lib/kernel/list.h"
-#include "vm/common-supp-pd.h"
-#include "userprog/pagedir.h"
-#include "threads/synch.h"
-#include "vm/carmina-swap.h"
 
-#include "common-vm.h"
+#include "lib/kernel/list.h"
+
+#include "userprog/pagedir.h"
+
+#include "threads/synch.h"
+#include "threads/palloc.h"
+
+#include "vm/common-supp-pd.h"
+#include "vm/carmina-swap.h"
+#include "vm/common-vm.h"
+
 
 void user_page_list_alloc(void)
 {
