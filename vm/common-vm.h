ad72dbab506ea842054f4d086d758794a4bf0c92
carminacodre <carminacodre@gmail.com>
2017-01-20 06:40:32 +0200

    modified:   vm/common-vm.h 	modified:   vm/moro-common.c

--- vm/common-vm.h
+++ vm/common-vm.h
@@ -23,27 +23,27 @@ void pte_forall(uint32_t *pd, int user_only, void (*foo)(uint32_t *));
 #define IS_MMAPPED(pte) ((pte) & PAGE_MMAPPED )
 
 /// sets free flag bits in pte to PAGE_LAZY_LOADED used by pte_forall
-inline void
+static inline void
 pte_set_lazy_loaded(uint32_t * pte)
 {
     *pte = *pte || PAGE_LAZY_LOADED;
 }
 
 /// sets free flag bits in pte to PAGE_SWAPPED used by pte_forall
-inline void
+static inline void
 pte_set_swapped(uint32_t * pte)
 {
     *pte = *pte || PAGE_SWAPPED;
 }
 
 /// sets free flag bits in pte to PAGE_MMAPED used by pte_forall
-inline void
+static inline void
 pte_set_mmaped(uint32_t * pte)
 {
     *pte = *pte || PAGE_MMAPPED;
 }
 
-inline void
+static inline void
 pte_clear_additional(uint32_t * pte)
 {
 	*pte = *pte & ~PAGE_MMAPPED & ~PAGE_LAZY_LOADED & ~PAGE_SWAPPED;
